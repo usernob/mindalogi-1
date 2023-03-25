ad6cc323ad0b2f631c0b2a267498fc7c715847f4
usernob <shamsulmaarif2016@gmail.com>
2023-03-25 21:49:10 +0700

    change config file

--- config.example.php
+++ config.example.php
@@ -2,8 +2,8 @@
 // define("BASE_URL", "https://greenie.loca.lt/greenie");
 define("BASE_URL", "http://localhost/mindalogi");
 define("ASSETS", "http://localhost/mindalogi/public/assets");
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "usernob");
+define("DB_PASS", "usernob");
 define("DB_NAME", "mindalogi");
 define("WEB_NAME", "Mindalogi");
