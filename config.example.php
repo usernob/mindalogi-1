39900ea8f40bf319f59ee342fc3055d9c50862d9
usernob <shamsulmaarif2016@gmail.com>
2023-03-27 02:47:19 +0700

    change config

--- config.example.php
+++ config.example.php
@@ -3,7 +3,7 @@
 $protocol = "https";
 define("BASE_URL", $protocol . "://" . $_SERVER["SERVER_NAME"]);
 define("ASSETS", $protocol . "://" . $_SERVER["SERVER_NAME"] . "/public/assets");
-define("DB_HOST", "127.0.0.1");
+define("DB_HOST", "database");
 define("DB_USER", "usernob");
 define("DB_PASS", "usernob");
 define("DB_NAME", "mindalogi");
